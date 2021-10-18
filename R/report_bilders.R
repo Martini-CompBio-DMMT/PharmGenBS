4a1c59fbcc50acfbccb083bb375c4aae5658ffae
Paolo Martini <paolo.cavei@gmail.com>
2021-10-18 08:52:02 +0200

    Link and requirement updates

--- R/report_bilders.R
+++ R/report_bilders.R
@@ -4,27 +4,22 @@
 #'
 #' @param file_info Get patients' file info form web_app generated through Prepare_data_Mac_v2.py
 #' @param file_genos Get patients' genotype info file form web_app generated through Prepare_data_Mac_v2.py
-#' @importFrom kableExtra kable
+#' @param useStoredAll_patients_report reuse stored data
 #' @importFrom rmarkdown render
 #' @export
 #'
-create_reports <- function(file_info, file_genos) {
-
-  require(kableExtra)
-  require(rmarkdown)
+create_reports <- function(file_info, file_genos, useStoredAll_patients_report=FALSE) {
   check_local_files()
   sample_info_table <- generate_sample_info_table(file_info, file_genos)
   patients <- sample_info_table$Sample
 
-  # if (!file.exists("all_patients_report.RData")){
-  #   all_patients_report <- lapply(patients, get_patient_clinical_guidelines, sample_info_table)
-  #   names(all_patients_report) <- patients
-  #   save(all_patients_report, file="all_patients_report.RData")
-  # } else {
-  #   load("all_patients_report.RData")
-  # }
-  all_patients_report <- lapply(patients, get_patient_clinical_guidelines, sample_info_table)
-  names(all_patients_report) <- patients
+  if (!file.exists("all_patients_report.RData") || !useStoredAll_patients_report) {
+    all_patients_report <- lapply(patients, get_patient_clinical_guidelines, sample_info_table)
+    names(all_patients_report) <- patients
+    save(all_patients_report, file = "all_patients_report.RData")
+  } else {
+    load("all_patients_report.RData")
+  }
 
   unformatted_reports <- all_patients_report
 
@@ -50,8 +45,7 @@ create_reports <- function(file_info, file_genos) {
       rmarkdown::render(input = "template.Rmd",
                         output_format = "pdf_document",
                         output_file = paste0(patient, ".pdf"),
-                        output_dir = output_dir,
-                        envir = new.env())
+                        output_dir = output_dir)
       detach("package:kableExtra", unload=TRUE)
     }
   }
@@ -61,14 +55,11 @@ create_reports <- function(file_info, file_genos) {
 #'
 #' @param file_info Get patients' file info form web_app generated through Prepare_data_Mac_v2.py
 #' @param file_genos Get patients' genotype info file form web_app generated through Prepare_data_Mac_v2.py
-#' @importFrom kableExtra kable
+#' @param useStoredAll_patients_report reuse stored data
 #' @importFrom rmarkdown render
 #' @export
 #'
-create_internal_reports <- function(file_info, file_genos) {
-
-  require(kableExtra)
-  require(rmarkdown)
+create_internal_reports <- function(file_info, file_genos, useStoredAll_patients_report=FALSE) {
   # file_info <- "../web_app/samples_info.tsv"
   # file_genos <- "../web_app/samples_genos.tsv"
   check_local_files()
@@ -76,8 +67,14 @@ create_internal_reports <- function(file_info, file_genos) {
   sample_info_table <- generate_sample_info_table(file_info, file_genos)
   patients <- sample_info_table$Sample
 
-  all_patients_report <- lapply(patients, get_patient_clinical_guidelines, sample_info_table, remove_last=T)
-  names(all_patients_report) <- patients
+  if (!file.exists("all_patients_report.RData") || !useStoredAll_patients_report) {
+    all_patients_report <- lapply(patients, get_patient_clinical_guidelines, sample_info_table, remove_last=T)
+    names(all_patients_report) <- patients
+    save(all_patients_report, file = "all_patients_report.RData")
+  } else {
+    load("all_patients_report.RData")
+  }
+
   unformatted_reports <- all_patients_report
 
   for (patient in names(unformatted_reports)) {
@@ -119,7 +116,7 @@ check_local_files <- function() {
   paths = c("images/rosso_small.png", "images/giallo_small.png",
     "images/verde_small.png", "images/white.png")
   for (name in paths) {
-    if (!file.exists(paths[name])) {
+    if (!file.exists(name)) {
       stop(paste0("Error: file ", name, " not found in '", getwd(),
                   "'. Please download dircetory images from 'from https://github.com/Martini-CompBio-DMMT/PharmGenBS_report_builder' and move it to the working directory"))
     }
